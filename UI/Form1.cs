0c4e3d9df5da71cf327c875005a9b4cd9bb67fcf
https://gitee.com/byman/MyDemo.git <https://gitee.com/byman/MyDemo.git>
2019-06-27 19:40:22 +0800

    热天

--- UI/Form1.cs
+++ UI/Form1.cs
@@ -16,5 +16,10 @@ namespace UI
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
     }
 }
